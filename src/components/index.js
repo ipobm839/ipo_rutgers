43f5fe8f8e854be3188be9e1072a89b8e22a939c
Brittany Morris <brittanymorris@Brittanys-MBP.verizon.net>
2022-02-18 13:56:39 -0500

    added top nav

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as NavTabs } from './NavTabs'
+export { default as Navbar } from './Navbar'
--- src/views/Tab1.js
+++ src/views/Tab1.js
@@ -2,13 +2,14 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { NavTabs } from "../components";
+import { Navbar, NavTabs } from "../components";
 
 const theme = createTheme();
 
 export default function Tab1() {
   return (
     <ThemeProvider theme={theme}>
+        <Navbar />
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         Tab 1
