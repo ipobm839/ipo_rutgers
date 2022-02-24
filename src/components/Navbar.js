57ccd2243444adfaf638ebb2951b7832409ce122
Brittany Morris <brittanymorris@nbp-25-141-154.nbp.ruw.rutgers.edu>
2022-02-24 15:23:14 -0500

    added drawer, working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
-import * as React from "react";
+import { useState } from "react";
+import Box from "@material-ui/core/Box";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import { makeStyles } from "@material-ui/core";
 import RUhorizontal from "../assets/ipo_horizontal.png";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -13,21 +23,76 @@ const useStyles = makeStyles((theme) => ({
     width: "50vw",
     padding: "8px",
   },
+  menuIcon: {
+    marginLeft: "20px",
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  list: {
+    width: 250,
+  },
+  profile: {
+    margin: "0 auto",
+  },
+  profileInfo: {
+    margin: "10px",
+  },
 }));
 
-const Navbar = () => {
+export default function Drawer() {
   const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
-    <AppBar className={classes.appBar} position="static">
-      <Toolbar disableGutters>
-        <img
-          src={RUhorizontal}
-          className={classes.RUhorizontal}
-          alt="RUhorizontal"
-        />
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar disableGutters>
+          <IconButton
+            onClick={() => setIsOpen(!isOpen)}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon className={classes.menuIcon} />
+          </IconButton>
+          <img
+            src={RUhorizontal}
+            className={classes.RUhorizontal}
+            alt="RUhorizontal"
+          />
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer
+        anchor="left"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        onOpen={() => {}}
+      >
+        <div className={classes.list}>
+          <Box textAlign="center" p={2}>
+            <Avatar alt="ipo_profile" src="#" className={classes.profile} />
+            <div className={classes.profileInfo}> Name Goes Here </div>
+          </Box>
+          <Divider />
+          <List>
+            <ListItem button onClick={() => navigate("/tab3")}>
+              <ListItemText primary={"Tools"} />
+            </ListItem>
+            <ListItem button onClick={() => navigate("/tab2")}>
+              <ListItemText primary={"Settings"} />
+            </ListItem>
+            <ListItem button onClick={() => navigate("/tab3")}>
+              <ListItemText primary={"Help"} />
+            </ListItem>
+            <ListItem button onClick={() => navigate("/tab2")}>
+              <ListItemText primary={"Logout"} />
+            </ListItem>
+          </List>
+        </div>
+      </SwipeableDrawer>
+    </div>
   );
-};
-export default Navbar;
+}
