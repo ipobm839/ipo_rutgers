ff74e8b9813649937515be74be8e6de4aa8adfac
Brittany Morris <brittany.morris@rutgers.edu>
2022-03-17 15:10:01 -0400

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Login, Tab1, Tab2, Tab3, NotFoundPage } from "./views/";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
